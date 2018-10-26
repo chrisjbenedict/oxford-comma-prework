aacd7a2272455ffae9128895ace21c5c3f90b963
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 21:30:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
   if array.size < 3
     array.join(' and ')
   else
-    array_without_last = array.shift(array.size - 1)
-    for item in array_without_last
-      joined_array_without_last = array_without_last.join(", ")
+    last_item = array.pop
+    array.join(', ') + ", and" + last_item
   end
-  return "#{joined_array_without_last}, and #{array.last}"
 end
